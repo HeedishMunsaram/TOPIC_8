7d809ee948d980e449a6009da91f1bef2dc69e51
Heedish Munsaram <54936388+HeedishMunsaram@users.noreply.github.com>
2019-09-27 15:16:22 +0400

    Add files via upload

---
+++ Q.1.py
@@ -0,0 +1,13 @@
+a = [5, 10, 15, 20, 25, 30, 35, 40]
+print("a[1]= ", a[1])
+print("a[4]= ", a[4])
+print("a[6]= ", a[6])
+print("a[0:3]= ", a[0:3])
+print("a[0:5]= ", a[0:5])
+print("a[0:7]= ",  a[0:7])
+print(a[2:6])
+print(a[3:8])
+print(a[0:5])
+print(a[5:])
+print(a[2:])
+
---
+++ Q.2.py
@@ -0,0 +1,25 @@
+d = {"ace" : "elite",
+     1 : "one",
+     "alligator" : "cousin of caiman",
+     2 : "two",
+     "ananas" : "king of fruit",
+     3 : "three",
+     "apple" : "eve's of fruit",
+     4 : "four",
+     "ball" : "round object",
+     5 : "five",
+     "car" : " 4 larou",
+     6 : "six",
+     "dog": " wow wow",
+     7 : "seven",
+     "elephant" : "big nose",
+     8 : "eight",
+     }
+
+print("The definition of car is " + d["car"]);
+print("The definition of apple is " + d["apple"]);
+print("The definition of ball is " + d["ball"]);
+print("The definition of elephant is " + d["elephant"]);
+print(d[1]);
+print(d[4]);
+print(d[6])
